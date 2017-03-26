14bb1c14a1d37a881c477b049e42103feda7beba
Kuriakin Zeng <zeng.kuriakin@gmail.com>
2017-03-26 14:11:31 +0000

    api completed

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var express = require('express');
+var moment = require('moment');
 var app = express();
+var date;
 
-app.get('/',function(req,res){
-    res.send('Hello World!');
+app.get('/:time',function(req,res){
+    if(moment(req.params.time, "X").format("X") == req.params.time)
+        date = moment(req.params.time,"X")
+
+    if(moment(req.params.time, "MMMM D, YYYY").format("MMMM D, YYYY") == req.params.time)
+        date = moment(req.params.time,"MMMM D, YYYY")
+    
+    if(date)
+        return res.json({ "unix": date.format("X"), "natural": date.format("MMMM D, YYYY") });
+    
+    res.json({ "unix": null, "natural": null });
 });
 
 app.listen(8080,function(){
-    console.log('I be listening to 8080');
+    console.log('Always be listen to 8080');
 });
